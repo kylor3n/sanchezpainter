ab0df6f17583eb5f0db573da31ae609dbd7433f8
quxncxrlos <quancarlos@icloud.com>
2016-06-28 23:29:56 -0600

    Add basic structure

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var cleancss = require('gulp-clean-css');
+
+gulp.task('jade', function () {
+    return gulp.src('src/index.jade')
+   .pipe(jade({ pretty: false }))
+   .pipe(gulp.dest('./'))
+});
+gulp.task('less', function () {
+    return gulp.src('src/main.less')
+    .pipe(less())
+    .pipe(autoprefixer({browsers: ['last 2 versions'],}))
+    .pipe(cleancss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('assets/css/'));
+});
